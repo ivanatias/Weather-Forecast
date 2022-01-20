455aa3283cdb92a196f30a5f537c6adf1980e10e
ivanatias <ivan.d.atias@gmail.com>
2022-01-20 01:29:48 -0400

    add: Main UI

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
+import Container from "react-bootstrap/Container";
+import { useQuery } from "react-query";
+import { useState } from "react";
+import Search from "./components/Search";
+import Weather from "./components/Weather";
+
+
 function App() {
+  const [query, setQuery] = useState({});
+
+  const { data, status, isFetching, isError, refetch } = useQuery("weather", {
+    enabled: false,
+    refetchOnWindowFocus: false,
+  })
+
+
   return (
-    <div className="App">
-      Project Setup
-    </div>
+    <Container className=" py-3">
+      <h1 className="text-center">Weather Forecast App</h1>
+      <Search query={query} setQuery={setQuery} fetch={refetch} />
+      {isFetching ? "Loading ..." :
+        isError ? "Error!" :
+          status === "success" ? data ? <Weather /> : "No results" : null}
+    </Container>
   );
 }
 
---
+++ src/components/Search.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Row, Col, Form, Button } from "react-bootstrap";
+import country from "../country-codes.json";
+
+const Search = ({ query, setQuery, fetch }) => {
+  const handleChange = ({ target }) => {
+    const { name, value } = target;
+    setQuery((prev) => ({
+      ...prev,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch();
+  };
+
+  return (
+    <Form onSubmit={handleSubmit} className="d-flex justify-content-center">
+      <Row>
+        <Col md={4}>
+          <Form.Group>
+            <Form.Control
+              type="text"
+              placeholder="Enter a city name"
+              name="city"
+              value={query.city || ""}
+              onChange={handleChange}
+            />
+          </Form.Group>
+        </Col>
+        <Col md={4}>
+          <Form.Group>
+            <Form.Select>
+              <option>Select country</option>
+              {country.map((c, i) => (
+                <option value={c.Code} key={i}>
+                  {c.Name}
+                </option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+        </Col>
+        <Col md={4} className="d-flex justify-content-center">
+          <Button type="submit">Check Weather</Button>
+        </Col>
+      </Row>
+    </Form>
+  );
+};
+
+export default Search;
